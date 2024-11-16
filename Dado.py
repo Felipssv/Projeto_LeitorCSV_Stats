1a28c5eb7b6e195be5c74092e0d39cd94400537f
Guga of Sosaria <gustavojatobaleite@gmail.com>
2024-11-16 15:58:19 -0300

    Adcionei os arquivos que contêm o objeto e as funções de arquivo

---
+++ Dado.py
@@ -0,0 +1,27 @@
+class Dado:
+    bissexto = False
+    horas = None
+    minutos = None
+
+    #OBSERVAÇÃO: "horas" e "minutos" devem ser strings!
+    def __init__(self, temperatura, dia, mes, ano, horas, minutos):
+        self.temperatura = temperatura
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
+        self.string_horario = f"{horas}:{minutos}"
+        if horas[0] == "0":
+            self.horas = int(horas[1])
+        else:
+            self.horas = int(horas)
+
+        if minutos[0] == "0":
+            self.minutos = int(minutos[1])
+        else:
+            self.minutos = int(minutos)
+
+        if (ano % 4 == 0 and ano % 100 != 0) or (ano % 100 == 0 and ano % 400 == 0):
+            self.bissexto = True
+        else:
+            self.bissexto = False
+      
